330c3162e594c6ffb8684d60df56620309f1af2f
SzczygielDev <32822239+SzczygielDev@users.noreply.github.com>
2024-06-18 19:23:02 +0200

    CORS config for development

---
+++ src/main/kotlin/pl/szczygieldev/ecommercebackend/infrastructure/config/WebConfig.kt
@@ -0,0 +1,19 @@
+package pl.szczygieldev.ecommercebackend.infrastructure.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig  {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:64427")
+            }
+        }
+    }
+}
