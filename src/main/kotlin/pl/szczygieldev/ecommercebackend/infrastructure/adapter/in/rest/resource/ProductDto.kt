01c7069140995556ff7b7b2788bb5736d8142bb6
SzczygielDev <32822239+SzczygielDev@users.noreply.github.com>
2024-08-05 22:12:01 +0200

    ProductFullDto for Admin frontend

--- src/main/kotlin/pl/szczygieldev/ecommercebackend/infrastructure/adapter/in/rest/resource/ProductDto.kt
+++ src/main/kotlin/pl/szczygieldev/ecommercebackend/infrastructure/adapter/in/rest/resource/ProductDto.kt
@@ -1,8 +1,5 @@
 package pl.szczygieldev.ecommercebackend.infrastructure.adapter.`in`.rest.resource
 
 import java.math.BigDecimal
-import java.time.Instant
 
-data class ProductDto(val productId:String,val title:String, val description: String,val price: BigDecimal, val priceChanges: List<ProductPriceChangeDto>){
-    data class ProductPriceChangeDto(val newPrice: BigDecimal, val  timestamp: Instant)
-}
+data class ProductDto(val productId:String,val title:String, val description: String,val price: BigDecimal)
