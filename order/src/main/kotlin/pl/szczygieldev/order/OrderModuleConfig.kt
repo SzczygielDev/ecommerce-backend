5e08cc8b9711789c0e3ee7f14d89b2ceb8887e75
SzczygielDev <32822239+SzczygielDev@users.noreply.github.com>
2025-01-04 12:22:18 +0100

    Added Spring package scanning configuration for Exposed

---
+++ order/src/main/kotlin/pl/szczygieldev/order/OrderModuleConfig.kt
@@ -0,0 +1,9 @@
+package pl.szczygieldev.order
+
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@AutoConfigurationPackage
+internal class OrderModuleConfig {
+}
---
+++ product/src/main/kotlin/pl/szczygieldev/product/ProductModuleConfig.kt
@@ -0,0 +1,9 @@
+package pl.szczygieldev.product
+
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@AutoConfigurationPackage
+internal class ProductModuleConfig {
+}
